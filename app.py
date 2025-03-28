2e9854e39906abc79f613a98728b1240273e61e9
Arthur-Philiposki <arthur.5943@aluno.pr.senac.br>
2025-03-28 20:40:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import datetime
 
-nome = int(input("Qual o nome: "))
+nome = st.text_input("Qual o Nome: ")
+st.write("O nome digitado é: ", nome)
+
+sobrenome = st.text_input("Qual o Sobrenome: ")
+st.write("O sobrenome digitado é: ", sobrenome)
+
+data = st.date_input("Quando é o Aniversario", value=None)
+st.write("Aniversario é na data:", data)
