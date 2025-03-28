beffa4127cadeb9be053c2fcb4b20000cb45e0fd
Arthur-Philiposki <arthur.5943@aluno.pr.senac.br>
2025-03-28 20:25:20 -0300

    Update app.py
    Exercício 1

--- app.py
+++ app.py
@@ -1,38 +1,18 @@
 import streamlit as st
-import pandas as pd
 
-st.header ("Cabeçalho")
-
-st.multiselect (
-  'Quais são suas corers favoritas?',
-  ['Verde', 'Amarelo', 'Vermelho', 'Azul'],
-  ['Amarelo', 'Vermelho',])
-
-st.color_picker("Escolha a Cor", "#00f900")
-st.feedback("stars")
+st.header ("Atividade Python")
 
 st.toggle ("Toggle")
-st.text_area ("Text de caixa Grande")
-st.text_input ("Text ants da Caixa", "Texto fraco na Caixa")
+
+st.text_input ("Adicione o Nome: ")
+st.text_input ("Adicione o Sobrenome: ")
+st.text_input ("Adicione a Data de nascimento: ", "Exemplo: xx/xx/xxxx")
+st.text_input ("Adicione o Salario: ")
 
 st.selectbox (
-  'Qual a sua cor favorita?', 
-  ('Azul', 'Vermelho', 'Verde'))
+  'Qual o Estado civil: ', 
+  ('Solteiro', 'Casado', 'Divorciado', 'Viúvo'))
 
-st.checkbox ('Sorvete')
-st.checkbox ('Banana')
-st.checkbox ('Chocolate')
-
-options = ["Norte", "Sul", "Leste", "Oeste"]
-selection = st.pills("Directions", options, selection_mode="multi")
-
-st.button ("Botão Salvar")
-
-df = pd.DataFrame (
-  [
-    {"command": "st.selectbox", "rating": 4, "is_widget": True},
-    {"command": "st.balloons", "rating": 5, "is_widget": False},
-    {"command": "st.time_input", "rating": 3, "is_widget": True},
-  ]
-  )
-edited_df = st.data_editor(df)
+st.selectbox (
+  'Qual o Sexo: ', 
+  ('Masculino', 'Feminino', 'Outro'))
