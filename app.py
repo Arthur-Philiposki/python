d703b32fe7c02479f1915c1b77babe72438460ab
Arthur-Philiposki <arthur.5943@aluno.pr.senac.br>
2025-04-02 19:27:14 -0300

    Update app.py
    Exercicio 1

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import datetime
 
-nome = st.text_input("Qual o Nome: ")
+nome = st.text_input("Qual o Nome: ") 
 st.write("O nome digitado é: ", nome)
 
 sobrenome = st.text_input("Qual o Sobrenome: ")
@@ -9,3 +9,23 @@ st.write("O sobrenome digitado é: ", sobrenome)
 
 data = st.date_input("Quando é o Aniversario", value=None)
 st.write("Aniversario é na data:", data)
+
+option = st.selectbox(
+    "Qual é o Estado civil? ",
+    ("Solteiro", "Casado", "Divorciado", "Viúvo"),
+    index=None,
+    placeholder="Seleção.",
+)
+st.write("O estado civil selecionado é:", option)
+
+
+option = st.selectbox(
+    "Qual é o seu sexo? ",
+    ("Masculino", "Feminino", "Outro"),
+    index=None,
+    placeholder="Seleção.",
+)
+st.write("O sexo selecionado é:", option)
+
+salario = st.text_input("Qual o Salario: ")
+st.write("O salario digitado é: ", salario)
